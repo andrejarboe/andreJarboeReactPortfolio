137af1b3d06c0f2ba0ecda4d32642ea705746ca2
andrejarboe <andrejarboe@gmail.com>
2018-02-22 16:20:13 -0500

    add portfolio sectio to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -18,16 +18,68 @@ const Home = () =>
 
       
       <p id='bio' className="flow-text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis et
-        tortor ac tincidunt. In euismod iaculis lobortis. Vestibulum posuere molestie
-        ipsum vel sollicitudin. Vestibulum venenatis pharetra mi, ut vestibulum elit
-        ultricies a. Vestibulum at mollis ex, ac consectetur massa. Donec nunc dui,
-        laoreet a nibh et, semper tincidunt nunc. Donec ac posuere tellus. Pellentesque
-        tempus suscipit velit sit amet bibendum.
+        Ever since I was a young boy I have been fascinated with outer space and the technology used to get there. I hope I get to experience space travel before my time is up. I also love to solve riddles and puzzles. This skill has helped me when the going gets tough in programming. I know there is always a solution to the problem I just might have to look at it from a different angle. I love learning web development and tutoring my classmates. I find that I have a more positive attitude than most people. The way I see it “You can complain and be nothing, or work and be something...” 
       </p>
     </div>
   </div>
   {/* Sample Portfolio */}
+  <section id="portfolio" className='row'>
+    <div className='container'>
+    <div className='col s12'>
+      <h5 className='center'>Portfolio</h5>
+    </div>
+      <div className='col s12 m4 l4'>
+        <div className='card'>
+          <div className="card-image">
+            <img className="activator" src={ProfilePic} />
+          </div>
+          <div class="card-content">
+            <span class="card-title">Card Title</span>          
+            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Github</a>
+            <a href="#">Live Demo</a>            
+          </div>
+        </div>
+      </div>
+
+      <div className='col s12 m4 l4'>
+        <div className='card'>
+          <div className="card-image">
+            <img className="activator" src={ProfilePic} />
+          </div>
+          <div class="card-content">
+            <span class="card-title">Card Title</span>          
+            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Github</a>
+            <a href="#">Live Demo</a>            
+          </div>
+        </div>
+      </div>
+
+      <div className='col s12 m4 l4'>
+        <div className='card'>
+          <div className="card-image">
+            <img className="activator" src={ProfilePic} />
+          </div>
+          <div class="card-content">
+            <span class="card-title">Card Title</span>          
+            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Github</a>
+            <a href="#">Live Demo</a>            
+          </div>
+        </div>
+      </div>
+      <div className='col s12'>
+      <h5 className='center'>View More</h5>
+    </div>
+    </div>
+  </section>
 </section>
 
 export default Home;
