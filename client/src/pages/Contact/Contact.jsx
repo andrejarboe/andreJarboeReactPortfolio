b187125d0938147e09796b76b474fd24e074ebd9
andrejarboe <andrejarboe@gmail.com>
2018-04-13 17:22:29 -0400

    working on contact page

--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -1,7 +1,42 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 
 class Contact extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      subject: '',
+      message: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  // state = {
+
+  // }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+    // const { name, email, subject, message } = this.state;
+
+    // if (this.state.name && this.state.email) {
+    axios.post("/api/post", {
+      name: this.state.name,
+      email: this.state.email,
+      message: this.state.message
+    })
+    // .then(res => this.loadBooks())
+    // .catch(err => console.log(err));
+    // }
+  }
+
   render() {
     return (
       <section id="contact">
@@ -10,26 +45,61 @@ class Contact extends Component {
             <div className="col m10 offset-m1 s12">
               <h2 className="center-align">Contact Me</h2>
               <div className="row">
-                <form method="POST" action="send" className="col m8 offset-m2 s12">
+                <form
+                  onSubmit={this.handleSubmit}
+                  className="col m8 offset-m2 s12"
+                >
                   <div className="row">
                     <div className="input-field col s12">
-                      <input id="name" type="text" />
-                      <label for="name">Name</label>
+                      <input
+                        value={this.state.name}
+                        id="name"
+                        type="text"
+                        onChange={this.handleChange} />
+                      <label for="name">Name:</label>
                     </div>
                     <div className="input-field col s12">
-                      <input id="email" type="email" className="form-input" />
-                      <label for="email">Email</label>
+                      <input
+                        value={this.state.email}
+                        id="email"
+                        type="email"
+                        className="form-input"
+                        type="text"
+                        onChange={this.handleChange} />
+                      <label for="email">Email:</label>
                     </div>
                     <div className="input-field col s12">
-                      <textarea id="message" className="materialize-textarea"></textarea>
-                      <label for="message">Message</label>
+                      <input
+                        value={this.state.subject}
+                        id="subject"
+                        type="text"
+                        onChange={this.handleChange} />
+                      <label for="name">Subject:</label>
+                    </div>
+                    <div className="input-field col s12">
+                      <textarea
+                        value={this.state.message}
+                        id="message"
+                        className="materialize-textarea"
+                        type="text"
+                        onChange={this.handleChange}
+                      ></textarea>
+                      <label for="message">Message:</label>
                     </div>
                   </div>
 
                   {/* // <div className="divider"></div> */}
                   <div className="row">
                     <div className="col m12">
-                      <p className="right-align"><button className="btn btn-large waves-effect waves-light" type="submit" name="action">Send Message</button></p>
+                      <p className="right-align">
+                        <button
+                          className="btn btn-large waves-effect waves-light"
+                          type="submit"
+                          name="action"
+                        >
+                          Send Message
+                        </button>
+                      </p>
                     </div>
                   </div>
                 </form>
